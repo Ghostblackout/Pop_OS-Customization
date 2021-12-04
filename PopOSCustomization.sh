a146dd91a0b6f6421a45474f23699866e446b8ec
Ghostblackout <95517271+Ghostblackout@users.noreply.github.com>
2021-12-04 00:47:22 -0500

    Add files via upload

---
+++ PopOSCustomization.sh
@@ -0,0 +1,21 @@
+clear
+sudo apt update
+sudo apt full-upgrade
+
+#this part Upgrade's Pop_OS!
+
+sudo apt install kde-standard -y
+sudo apt-get install lightdm -y
+
+#this prat install's kde plasma and lightdm
+
+sudo apt install neofetch -y
+sudo apt install mpv -y
+sudo apt install steam
+git clone https://github.com/pystardust/ani-cli.git
+cd ani-cli
+sudo make
+
+clear
+
+sudo restart
---
+++ README.md
@@ -0,0 +1,2 @@
+Pop_OS-Customization
+Is a program that install's stuff that i like and need with Pop_OS!
