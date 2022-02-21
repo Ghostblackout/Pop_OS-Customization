40f1b297b59599cb036993227846767086db9338
Ghostblackout <95517271+Ghostblackout@users.noreply.github.com>
2022-02-21 10:22:42 -0500

    Update PopOSCustomization.sh

--- PopOSCustomization.sh
+++ PopOSCustomization.sh
@@ -1,13 +1,15 @@
+#this part Upgrade's Pop_OS!
+
 clear
 sudo apt update
 sudo apt full-upgrade
 
-#this part Upgrade's Pop_OS!
+#this prat install's kde plasma and lightdm
 
 sudo apt install kde-standard -y
 sudo apt-get install lightdm -y
 
-#this prat install's kde plasma and lightdm
+#This installs the stuff's
 
 sudo apt install neofetch -y
 sudo apt install mpv -y
